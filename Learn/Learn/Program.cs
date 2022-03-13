cce30000c8b03cff44245760f2af7d5e8e7d12c8
Айрат Арсланов <ayrat.arslanovd@mail.ru>
2022-03-13 14:18:19 +0300

    new

---
+++ Learn/Learn/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    public class Program
+    {
+		static void Main()
+		{
+			System.Console.WriteLine(Min(4, 2, 3));
+		}
+
+		static int Min(int a, int b, int c)
+		{
+			return Math.Min(a, Math.Min(b, c));
+		}
+	}
+}
